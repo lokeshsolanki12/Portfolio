47e3f8b884c8167c0e2aa907872998f9d1791693
Lokesh Solanki <lokesh.solanki7014.in>
2026-04-10 00:45:55 +0530

    backend ready

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,8 +5,7 @@ from routes.contact import contact_bp
 app = Flask(__name__)
 CORS(app)
 
-# Register Blueprint
 app.register_blueprint(contact_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run()
